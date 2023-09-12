7bf7bc254726c8873904e1c672a9ebf92c9821d7
a5-stable <sh07e1916@gmail.com>
2023-09-12 22:14:25 +0900

    fix clock variety

--- lib/emoji_clock/clock_variety.rb
+++ lib/emoji_clock/clock_variety.rb
@@ -1,6 +1,6 @@
 module ClockVariety
   EMOJI_CLOCK_CODE_POINT = 128336
-  ALERM_CLOCK = "\u{23F0}"
+  ALARM_CLOCK = "\u{23F0}"
   TIMER_CLOCK = "\u{23F2}"
   WATCH_CLOCK = "\u{231A}"
   STOP_WATCH_CLOCK = "\u{23F1}"
@@ -8,59 +8,31 @@ module ClockVariety
   HOUR_GLASS_UNDONE = "\u{23F3}"
   MANTELPIECE_CLOCK = "\u{1F570}"
 
-  def self.alerm
-    ALERM_CLOCK
+  def alarm
+    ALARM_CLOCK
   end
 
-  def self.timer
+  def timer
     TIMER_CLOCK
   end
 
-  def self.watch
+  def watch
     WATCH_CLOCK
   end
 
-  def self.stop_watch
+  def stop_watch
     STOP_WATCH_CLOCK
   end
 
-  def self.hourglass_done
+  def hourglass_done
     HOUR_GLASS_DONE
   end
 
-  def self.hourglass_undone
+  def hourglass_undone
     HOUR_GLASS_UNDONE
   end
 
-  def self.mantelpiece
-    MANTELPIECE_CLOCK
-  end
-
-  def self.alerm
-    ALERM_CLOCK
-  end
-
-  def self.timer
-    TIMER_CLOCK
-  end
-
-  def self.watch
-    WATCH_CLOCK
-  end
-
-  def self.stop_watch
-    STOP_WATCH_CLOCK
-  end
-
-  def self.hourglass_done
-    HOUR_GLASS_DONE
-  end
-
-  def self.hourglass_undone
-    HOUR_GLASS_UNDONE
-  end
-
-  def self.mantelpiece
+  def mantelpiece
     MANTELPIECE_CLOCK
   end
 end
